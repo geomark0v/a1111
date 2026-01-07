287ce2e2174cbe768849548b73f21709c4901bd7
mac <mac@2345676.local>
2026-01-07 20:29:00 +0300

    config

--- config.py
+++ config.py
@@ -1,25 +1,33 @@
 # config.py
+# Базовый путь на RunPod Network Volume
 BASE = "/runpod-volume"
 MODEL_DIR = f"{BASE}/models"
 
-# SDXL
+# ---------------- SDXL модели ----------------
 SDXL_FILES = [
+    "cyberrealisticPony_v141.safetensors",
+    "cyberrealisticPony_v141 (1).safetensors",
+    "cyberrealisticPony_v150bf16.safetensors",
     "cyberrealisticPony_v150.safetensors",
-    "cyberrealisticPony_v141.safetensors"
 ]
 
-# ControlNet / IP-Adapter
-CONTROLNET_FILES = [
+# ---------------- ControlNet / IP-Adapter ----------------
+CONTROLNET_REPO = "https://github.com/Mikubill/sd-webui-controlnet"
+
+IP_ADAPTER_FILES = [
     "ip-adapter-faceid-plusv2_sdxl.bin",
     "ip-adapter-faceid-plusv2_sdxl_lora.safetensors",
     "ip-adapter-plus-face_sdxl_vit-h.safetensors",
-    "ip-adapter-plus_sdxl_vit-h.safetensors",
+    "ip-adapter-plus_sdxl_vit-h (1).safetensors",
+    "ip-adapter_sdxl_vit-h (1).safetensors",
     "clip_h.pth",
     "ip_adapter_instant_id_sdxl.bin",
     "control_instant_id_sdxl.safetensors",
 ]
 
-# ReActor
+# ---------------- ReActor ----------------
+REACTOR_REPO = "https://codeberg.org/Gourieff/sd-webui-reactor.git"
+
 REACTOR_FILES = [
     "inswapper_128.onnx",
     "1k3d68.onnx",
@@ -31,9 +39,9 @@ REACTOR_FILES = [
     "w600k_r50.onnx",
 ]
 
-# GFPGAN / CodeFormer
+# ---------------- GFPGAN / CodeFormer ----------------
 GFPGAN_FILES = ["GFPGANv1.4.pth"]
-CODEFORMER_FILES = ["codeformer.pth", "codeformer-v0.1.0.pth"]
-
-# ADetailer
-ADETAILER_FILES = ["face_yolov8n.pt"]
+CODEFORMER_FILES = [
+    "codeformer.pth",
+    "codeformer-v0.1.0.pth"
+]
