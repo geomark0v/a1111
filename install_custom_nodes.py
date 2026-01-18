65af1adf7d24da8d43b61fd25261e4a30187a4f6
mac <mac@2345676.local>
2026-01-18 18:54:03 +0300

    start

--- install_custom_nodes.py
+++ install_custom_nodes.py
@@ -73,7 +73,7 @@ def install_repo(repo_url):
     reqs_path = repo_path / "requirements.txt"
     if reqs_path.exists():
         print(f"[INSTALL] requirements.txt для {repo_name}")
-        run_command([sys.executable, "-m", "uv", "pip", "install", "-r", str(reqs_path)])
+        run_command([sys.executable, "-m", "pip", "install", "-r", str(reqs_path)])
 
     # Запуск install.py, если есть
     install_py = repo_path / "install.py"
